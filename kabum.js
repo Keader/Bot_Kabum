b425fe44621b600e737b864281d894aba47f6429
Keader <keader.android@gmail.com>
2019-10-26 14:47:43 -0300

    Fixed bugs of UTF8 and \n problem

--- kabum.js
+++ kabum.js
@@ -1,24 +1,21 @@
 var  request = require('request');
 var  cheerio = require('cheerio');
 var fs = require('fs');
-var utf8 = require('utf8');
 
-request('https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=rtx+2070+super', function(err, res, body)
+request('https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=rtx+2070+super', { encoding: 'binary'} ,  function(err, res, body)
 {
     if (err)
         console.log('Error: ' + err);
     else
     {
 
-        const $ = cheerio.load(body, { decodeEntities: false });
-        $('.box_page').each(function()
+        const $ = cheerio.load(body);
+        var box = $('.H-titulo a');
+        box.each(function()
         {
-           var titulo = $(this).find('.H-titulo a').text().trim();
-
-            fs.appendFile('kabum.txt', titulo + '\r\n','utf8', (err) => {
-                if (err) throw err;
-                console.log('The "data to append" was appended to file!');
-            });
+           var titulo = $(this).text();
+           var link = $(this).attr('href');
+           console.log(titulo + ' ' + link);
         });
     }
 });
