a4969c11cb5fe607ed47ffcd36519211d234c451
Keader <keader.android@gmail.com>
2019-10-26 15:44:24 -0300

    Added price and available check

--- kabum.js
+++ kabum.js
@@ -1,6 +1,5 @@
-var  request = require('request');
-var  cheerio = require('cheerio');
-var fs = require('fs');
+const  request = require('request');
+const  cheerio = require('cheerio');
 
 request('https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=rtx+2070+super', { encoding: 'binary'} ,  function(err, res, body)
 {
@@ -10,12 +9,25 @@ request('https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=rt
     {
 
         const $ = cheerio.load(body);
-        var box = $('.H-titulo a');
-        box.each(function()
+        $('.listagem-box').each(function()
         {
-           var titulo = $(this).text();
-           var link = $(this).attr('href');
-           console.log(titulo + ' ' + link);
+            // Search by Title and Link stuffs
+            const titleBlock = $(this).find('.H-titulo a');
+            const title = titleBlock.text();
+            const link = titleBlock.attr('href');
+            console.log(title + ' : ' + link + '\n');
+
+            // Search Price (with 15% discount)
+            const priceFull = $(this).find('.listagem-preco').text();
+            console.log('Preço: ' + priceFull + '\n');
+
+            // Check if is available.
+            const image = $(this).find('.listagem-bots').find('img').attr('src');
+            const available = !image.includes('comprar_off');
+            console.log(available ? 'Produto está disponível !\n' : 'Produto está Indisponível ! \n');
+
+            //Todo: Need check parse all link pages, to check if has promotion or not.
+
         });
     }
 });
